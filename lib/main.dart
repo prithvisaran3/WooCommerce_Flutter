9c919052bb82c054e8059135e293f7441a27bc6f
Prithvi Saran S <prithvisaran.s@gmail.com>
2023-06-20 12:17:53 +0530

    Push Notification(changed)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:steels/app/services/notification.dart';
@@ -13,12 +14,27 @@ class MyHttpOverrides extends HttpOverrides {
           (X509Certificate cert, String host, int port) => true;
   }
 }
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  // startMessage();
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   runApp(
+  //     const MaterialApp(
+  //       debugShowCheckedModeBanner: false,
+  //       home: TrueCallerOverlay(),
+  //     ),
+  //   );
 
+  print('Handling a background message ${message.messageId}');
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   HttpOverrides.global = MyHttpOverrides();
   initMessaging();
+
   runApp(const MyApp());
 }
 
